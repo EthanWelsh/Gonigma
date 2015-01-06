409898d75c2c8c1830a3b16ed598fede1f0f26d3
EthanWelsh <welshej@gmail.com>
2015-01-05 22:51:20 -0500

    add relfector component

---
+++ Gonigma/src/main/Reflector.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"unicode"
+)
+
+type Reflector struct {
+	slots []rune
+}
+
+// reads a reflector file from the given path and returns a rotor of that configuration
+func (r Reflector) NewReflector(file string) Reflector {
+
+	var err error
+
+	message, err := ioutil.ReadFile(file) // Read the message from the message file
+
+	if err != nil {
+		fmt.Println("Error reading from file!!!")
+		fmt.Println(err)
+	}
+
+	s := string(message[:])
+	split := strings.Split(s, "\n")
+
+	r.slots = make([]rune, len(split)*2)
+
+	for i := range split {
+
+		pair := strings.Split(split[i], " ")
+
+		first := pair[0]
+		second := pair[1]
+
+		firstRune := ([]rune(first))[0]
+		secondRune := ([]rune(second))[0]
+
+		r.slots[firstRune-'A'] = secondRune
+		r.slots[secondRune-'A'] = firstRune
+	}
+
+	return r
+}
+
+// given a character, will translate that character and return the translated character
+func (r *Reflector) Translate(c rune) (ret rune) {
+
+	c = unicode.ToUpper(c)
+	index := c - 'A'
+	return r.slots[index]
+}
